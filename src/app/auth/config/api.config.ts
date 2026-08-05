56f125b8161383a1f6dbf1f146f102ae6ea442ab
asfakwork07 <asfak.work07@gmail.com>
2026-08-05 23:54:03 +0530

    rebder service issus fixed

--- src/app/auth/config/api.config.ts
+++ src/app/auth/config/api.config.ts
@@ -1,9 +1,9 @@
-export const API_URL =
-  window.location.hostname === 'localhost'
-    ? 'http://localhost:8080/api'
-    : 'https://13.49.241.55/api';
-
 // export const API_URL =
 //   window.location.hostname === 'localhost'
-//     ? 'http://13.49.241.55/api'
+//     ? 'http://localhost:8080/api'
 //     : 'https://employee-management-backend-spring-boot-1.onrender.com/api';
+
+export const API_URL =
+  window.location.hostname === 'localhost'
+    ? 'http://13.49.241.55/api'
+    : 'https://employee-management-backend-spring-boot-1.onrender.com/api';
